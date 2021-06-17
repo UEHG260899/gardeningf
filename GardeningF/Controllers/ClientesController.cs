63584cf0fefbb08af626c2285da4d3696ca944bb
Uriel <urielenrique99@gmail.com>
2021-06-17 10:37:43 -0500

    agregado : validacion de tarjeta

--- GardeningF/Controllers/ClientesController.cs
+++ GardeningF/Controllers/ClientesController.cs
@@ -7,6 +7,7 @@ using System.Net;
 using System.Web;
 using System.Web.Mvc;
 using GardeningF.Models;
+using System.Text;
 
 namespace GardeningF.Controllers
 {
@@ -49,6 +50,22 @@ namespace GardeningF.Controllers
         [ValidateAntiForgeryToken]
         public ActionResult Create(string nombre_cliente, string app_cliente, string apm_cliente, string telefono_cliente, string correo_cliente, string estado, string municipio, string calle, string colonia, string cp, string noExt, string tarjeta, string cvv, string fecha, string tipoT)
         {
+            /*Cliente cliente = new Cliente();
+            int id = 0;
+            if(!(db.Cliente.Max(c => (int?)c.id_cliente) == null))
+            {
+                id = db.Cliente.Max(c => c.id_cliente);
+            }
+            else
+            {
+                id = 0;
+            }
+            id++;
+
+            if (tarjetaValida(tarjeta, tipoT, fecha, cvv))
+            {
+
+            }*/
             return RedirectToAction("Index", "Home");
         }
 
@@ -116,6 +133,100 @@ namespace GardeningF.Controllers
                 db.Dispose();
             }
             base.Dispose(disposing);
+        }
+
+
+        //Validación de fecha de tarjeta
+        private bool tarjetaValida(string tarjeta, string tipoTarjeta, string fecha, string cvv)
+        {
+            bool bandera = validar(tarjeta);
+
+            if (bandera)
+            {
+                if((tarjeta.StartsWith("4")) && (tipoTarjeta.Equals("Visa")))
+                {
+                    bandera = true;
+                }else if ((tarjeta.StartsWith("5")) && (tipoTarjeta.Equals("Master")))
+                {
+                    bandera = true;
+                }else if ((tarjeta.StartsWith("3")) && (tipoTarjeta.Equals("American")))
+                {
+                    bandera = true;
+                }
+                else
+                {
+                    bandera = false;
+                }
+
+                DateTime hoy = new DateTime();
+                string[] fechaTarjeta = fecha.Split('-');
+                if(Convert.ToInt32(fechaTarjeta[0]) >= hoy.Year)
+                {
+                    if(Convert.ToInt32(fechaTarjeta[1]) > hoy.Month)
+                    {
+                        bandera = true;
+                    }
+                    else
+                    {
+                        bandera = false;
+                    }
+                }
+                else
+                {
+                    bandera = false;
+                }
+
+            }
+
+            return bandera;
+        }
+
+        //Algoritmo de Luhn
+        private bool validar(string tarjeta)
+        {
+            bool retorna = true;
+            StringBuilder soloDigitos = new StringBuilder();
+
+            foreach(Char c in tarjeta)
+            {
+                if (Char.IsDigit(c))
+                {
+                    soloDigitos.Append(c);
+                }
+            }
+
+            //Hay más o menos digitos de los esperados
+            if (soloDigitos.Length > 18 || soloDigitos.Length < 15) return false;
+
+            //varibles auxiliares en el algoritmo
+            int sum = 0;
+            int digito = 0;
+            int addEnd = 0;
+            bool porDos = false;
+
+            for(int i = soloDigitos.Length - 1; i >= 0; i--)
+            {
+                digito = Int32.Parse(soloDigitos.ToString(i, 1));
+                if (porDos)
+                {
+                    addEnd = digito * 2;
+                    if(addEnd > 9)
+                    {
+                        addEnd -= 9;
+                    }
+                }
+                else
+                {
+                    addEnd = digito;
+                }
+
+                sum += addEnd;
+
+                porDos = !porDos;
+            }
+
+            retorna = ((sum % 10) == 0);
+            return retorna;
         }
     }
 }
