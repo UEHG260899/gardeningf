3c94750303835a70d3c02b77666457c296d7db72
Uriel <urielenrique99@gmail.com>
2021-06-16 22:39:49 -0500

    Commit inicial

---
+++ GardeningF/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GardeningF.Startup))]
+namespace GardeningF
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
