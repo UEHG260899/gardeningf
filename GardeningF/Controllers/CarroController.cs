874eed59b0aca726e75bb05b3045de7434c65ff7
CarlosSerranoV <carlosjsv251999@gmail.com>
2021-06-18 01:30:13 -0500

    feat: se agrego seccion de productos destacados en vista carro

--- GardeningF/Controllers/CarroController.cs
+++ GardeningF/Controllers/CarroController.cs
@@ -15,6 +15,22 @@ namespace GardeningF.Controllers
         // GET: Carro
         public ActionResult Index()
         {
+            //Items del carrito
+            if (Session["itemCarro"] == null)
+            {
+                Session["itemCarro"] = 0;
+            }
+            //Código para los productos
+            Random ran = new Random();
+            int num = ran.Next(1, 60);
+            int num2 = ran.Next(1, 60);
+            int num3 = ran.Next(1, 60);
+            int num4 = ran.Next(1, 60);
+            var producto = db.Producto.SqlQuery(
+                @"SELECT * FROM dbo.Producto WHERE id_producto IN (@id1,@id2,@id3,@id4)",
+                new SqlParameter("@id1", num), new SqlParameter("@id2", num2), new SqlParameter("@id3", num3), new SqlParameter("@id4", num4)).ToList();
+            //Viewbag
+            ViewBag.prod = producto;
             return View();
         }
 
