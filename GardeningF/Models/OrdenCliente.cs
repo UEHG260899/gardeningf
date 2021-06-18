1a5c17bd613395bae676bfd156879fcea5c554d2
Uriel <urielenrique99@gmail.com>
2021-06-18 18:36:32 -0500

    cambiado : modelo

--- GardeningF/Models/OrdenCliente.cs
+++ GardeningF/Models/OrdenCliente.cs
@@ -26,15 +26,15 @@ namespace GardeningF.Models
         public Nullable<int> total { get; set; }
         public int id_cliente { get; set; }
         public int id_dir_entrega { get; set; }
-        public int id_paqueteria { get; set; }
+        public Nullable<int> id_paqueteria { get; set; }
         public Nullable<int> num_guia { get; set; }
         public Nullable<System.DateTime> fecha_envio { get; set; }
         public Nullable<System.DateTime> fecha_entrega { get; set; }
     
         public virtual Cliente Cliente { get; set; }
+        public virtual Direccion Direccion { get; set; }
         public virtual Paqueteria Paqueteria { get; set; }
         [System.Diagnostics.CodeAnalysis.SuppressMessage("Microsoft.Usage", "CA2227:CollectionPropertiesShouldBeReadOnly")]
         public virtual ICollection<OrdenProducto> OrdenProducto { get; set; }
-        public virtual Direccion Direccion { get; set; }
     }
 }
