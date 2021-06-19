b7a13c278af7e8d28b2eaf2dfa2791477c5233cc
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-06-19 00:09:02 -0500

    feat: Tabla empleados en Admin

---
+++ GardeningF/Controllers/AdminController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Data.Entity;
+using System.Web.Mvc;
+using GardeningF.Models;
+
+namespace GardeningF.Controllers
+{
+    public class AdminController : Controller
+    {
+        private contextoGardeningF db = new contextoGardeningF();
+        // GET: Admin
+        public ActionResult Index()
+        {
+            var empleado = db.Empleado.Include(e => e.Departamento);
+            var departamentos = db.Departamento.ToList();
+            ViewBag.empleados = empleado.ToList();
+            ViewBag.departamentos = departamentos;
+            return View();
+        }
+    }
+}
