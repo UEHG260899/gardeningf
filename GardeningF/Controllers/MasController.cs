c3b05f5e4f99cfc5a9052cd9c1aaac14d17492cb
Eduardo Apodaca <eduardo1ja99@gmail.com>
2021-06-20 02:59:45 -0500

    Tarjeta

---
+++ GardeningF/Controllers/MasController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace GardeningF.Controllers
+{
+    public class MasController : Controller
+    {
+        // GET: Mas
+        public ActionResult Tarjeta()
+        {
+            return View();
+        }
+    }
+}
